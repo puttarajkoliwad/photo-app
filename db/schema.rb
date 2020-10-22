e69b3cab0c3b020df77fb60c0120d8bf8b1ea162
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2020-10-22 12:41:43 +0530

    till payments part. Configure keys for production

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_202746) do
+ActiveRecord::Schema.define(version: 2020_10_21_093240) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
